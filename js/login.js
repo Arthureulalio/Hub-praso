32654c91804c2f7ab027324c606346dd2fdcfd98
anacarolinx <ana.00000847943@unicap.br>
2024-09-13 17:02:14 -0300

    login2

--- js/login.js
+++ js/login.js
@@ -1,13 +1,15 @@
 // Função de redirecionamento ao clicar no botão de login
 document.getElementById('loginButton').addEventListener('click', function() {
-    // Simular validação de login (opcional: aqui você pode validar o formulário antes de redirecionar)
+    // Pegar os valores dos campos de email e senha
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if (email && password) {
+    // Verificar se os valores correspondem às credenciais corretas
+    if (email === 'teste@praso.com.br' && password === 'praso284546') {
         // Redirecionar para a página home.html
         window.location.href = "home.html";
     } else {
-        alert('Por favor, insira o email e a senha.');
+        // Exibir mensagem de erro caso as credenciais estejam incorretas
+        alert('Email ou senha incorretos.');
     }
 });
